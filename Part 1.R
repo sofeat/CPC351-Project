a7281b339b0325ebd392128e75da6ce791882756
Sofea Taufik <94227896+sofeat@users.noreply.github.com>
2024-01-20 21:32:54 +0800

    Update Part 1.R
    Performed data preparation

--- Part 1.R
+++ Part 1.R
@@ -25,3 +25,24 @@ df$`Product Name` <- factor(df$`Product Name`)
 
 # getting the values in attribute segment
 summary(df$Segment)
+
+# Data preparation
+
+# Check for null values
+which(is.na(df)) # no null values
+
+# Check for duplicate rows
+duplicate_indices <- which(duplicated(df))
+
+# View duplicate rows
+duplicate_rows <- df[duplicate_indices, ] # no duplicate rows
+
+# Remove Row ID attribute because Order ID is already unique
+df <- df[, !(names(df) %in% c("Row ID"))]
+
+# Performing data normalisation for scale consistency
+
+# Using range normalization
+df$Sales <- (df$Sales - min(df$Sales)) / (max(df$Sales) - min(df$Sales))
+df$Quantity <- (df$Quantity - min(df$Quantity)) / (max(df$Quantity) - min(df$Quantity))
+df$Profit <- (df$Profit - min(df$Profit)) / (max(df$Profit) - min(df$Profit))
