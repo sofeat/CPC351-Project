09e1aa81809d6732d2a078c970c04e2ad7a94ea5
Sofea Taufik <94227896+sofeat@users.noreply.github.com>
2024-01-26 22:52:51 +0800

    Update Part 1.R
    change the order of splitting dataset and data preprocessing

--- Part 1.R
+++ Part 1.R
@@ -26,27 +26,27 @@ df$`Product Name` <- factor(df$`Product Name`)
 # getting the values in attribute segment
 summary(df$Segment)
 
-# Splitting dataset train-test
-sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
-train_df  <- df[sample, ]
-test_df   <- df[!sample, ]
-
 # Data preparation
 
 # Check for null values
-which(is.na(train_df)) # no null values
+which(is.na(sales)) # no null values
 
 # Check for duplicate rows
-duplicate_indices <- which(duplicated(train_df))
+duplicate_indices <- which(duplicated(sales))
 
 # View duplicate rows
-duplicate_rows <- train_df[duplicate_indices, ] # no duplicate rows
+duplicate_rows <- sales[duplicate_indices, ] # no duplicate rows
 
 # Remove Row ID attribute because Order ID is already unique
-train_df <- train_df[, !(names(train_df) %in% c("Row ID"))]
+clean_sales <- sales[, !(names(sales) %in% c("Row ID"))]
 
 # Performing data normalisation for scale consistency
 # Using range normalization
-train_df$Sales <- (train_df$Sales - min(train_df$Sales)) / (max(train_df$Sales) - min(train_df$Sales))
-train_df$Quantity <- (train_df$Quantity - min(train_df$Quantity)) / (max(train_df$Quantity) - min(train_df$Quantity))
-train_df$Profit <- (train_df$Profit - min(train_df$Profit)) / (max(train_df$Profit) - min(train_df$Profit))
+clean_sales$Sales <- (clean_sales$Sales - min(clean_sales$Sales)) / (max(clean_sales$Sales) - min(clean_sales$Sales))
+clean_sales$Quantity <- (clean_sales$Quantity - min(clean_sales$Quantity)) / (max(clean_sales$Quantity) - min(clean_sales$Quantity))
+clean_sales$Profit <- (clean_sales$Profit - min(clean_sales$Profit)) / (max(clean_sales$Profit) - min(clean_sales$Profit))
+
+# Splitting data set train-test
+sample <- sample(c(TRUE, FALSE), nrow(clean_sales), replace=TRUE, prob=c(0.7,0.3))
+train_sales  <- clean_sales[sample, ]
+test_sales   <- clean_sales[!sample, ]
