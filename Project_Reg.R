4844bf6cc93da2e7dd8a5768b8c69dd25d7bbcd8
serenespringbloom <123131645+serenespringbloom@users.noreply.github.com>
2024-01-28 18:12:47 +0800

    Update Project_Reg.R

--- Project_Reg.R
+++ Project_Reg.R
@@ -23,25 +23,19 @@ head(exchange)
 # Getting the class for target variable: myr_usd
 summary(exchange$myr_usd)
 
-
 # Section II: Data Preparation ====
 
-# Check for null values
-which(is.na(exchange)) # no null values
+# Check data types
+print(sapply(exchange, class))
 
-# Check for duplicate rows
-which(duplicated(exchange)) # no duplicate rows
-
-# Display the structure of the dataset to view each variables data type
-str(exchange)
-
-# From the summary and structure, we can see there there's only one date column 
-# while the rest are numeric. Hence, remove the "date" column 
+# Remove the "date" column (if necessary)
 cleaned_exchange <- exchange[, !names(exchange) == "date"]
 
 # Display the summary statistics of the cleaned dataset
 summary(cleaned_exchange)
 
+# Performing data normalisation for scale consistency
+# Using range normalization
 normalize <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
 }
@@ -49,14 +43,9 @@ normalize <- function(x) {
 # Apply Min-Max scaling to your data frame
 normalized_exchange <- as.data.frame(lapply(cleaned_exchange, normalize))
 
-# Split the dataset into training and test sets
-set.seed(123) # for reproducibility
-sample <- sample(c(TRUE, FALSE), nrow(cleaned_exchange), replace=TRUE, prob=c(0.7, 0.3))
-train_exchange <- cleaned_exchange[sample, ]
-test_exchange <- cleaned_exchange[!sample, ]
+summary(normalized_exchange)
 
-
-# Visualize the dataset ====
+# Visualize the dataset
 
 cor_matrix <- cor(normalized_exchange)
 corrplot::corrplot(cor_matrix, method = "color", type = "upper", order = "hclust", tl.cex = 0.7)
@@ -65,9 +54,7 @@ corrplot::corrplot(cor_matrix, method = "color", type = "upper", order = "hclust
 hist(normalized_exchange$myr_usd, main = "Distribution of myr_usd", xlab = "myr_usd")
 
 # Create a figure with multiple plots
-
 # Scatterplot Matrix for key features
-
 # Scatterplot Matrix for key features (Set 1)
 pairs(normalized_exchange[c("myr_usd", "myr_rmb", "myr_sgd", "myr_eur", "myr_jpy")], 
       main = "Scatterplot Matrix: Key Features (Set 1)",
@@ -99,95 +86,100 @@ pairs(normalized_exchange[c("myr_usd", "myr_chf", "myr_cad", "myr_rub")],
       pch = 16, col = "orange")
 
 # Section III: Model Planning and Development ====
+set.seed(123)
+# Split the dataset into training and test sets
+sample <- sample(c(TRUE, FALSE), nrow(normalized_exchange), replace=TRUE, prob=c(0.7, 0.3))
+train <- normalized_exchange[sample, ]
+test <- normalized_exchange[!sample, ]
 
 # Fit multiple linear regression models
 linear_model1 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_jpy + myr_twd + myr_thb + myr_idr + myr_hkd + myr_krw +
                       myr_vnd + myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_gbp + myr_brl + myr_mxn +
-                      myr_bdt + myr_chf + myr_cad + myr_rub, data = train_exchange)
+                      myr_bdt + myr_chf + myr_cad + myr_rub, data = train)
 summary(linear_model1)
 
-# Remove the highest p-value non-significant feature (myr_thb)
-
-linear_model2 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_jpy+myr_twd+myr_idr+myr_hkd+myr_krw+
-                      myr_vnd+myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_try+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad+myr_rub, data = train_exchange)
-summary(linear_model2)
-
-# Remove the highest p-value non-significant feature (myr_jpy)
-
-linear_model3 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_twd+myr_idr+myr_hkd+myr_krw+
-                      myr_vnd+myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_try+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad+myr_rub, data = train_exchange)
-summary(linear_model3)
-
 # Remove the highest p-value non-significant feature (myr_rub)
+# Repeat the process for other linear models (linear_model2, linear_model3, etc.)
 
-linear_model4 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_twd+myr_idr+myr_hkd+myr_krw+
-                      myr_vnd+myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_try+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
-summary(linear_model4)
+linear_model2 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_jpy + myr_twd + myr_thb + myr_idr + myr_hkd + myr_krw +
+                      myr_vnd + myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_gbp + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model2)
 
 # Remove the highest p-value non-significant feature (myr_vnd)
 
-linear_model5 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_twd+myr_idr+myr_hkd+myr_krw+
-                      myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_try+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
+linear_model3 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_jpy + myr_twd + myr_thb + myr_idr + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_gbp + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model3)
+
+# Remove the highest p-value non-significant feature (myr_jpy)
+
+linear_model4 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_twd + myr_thb + myr_idr + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_gbp + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model4)
+
+# Remove the highest p-value non-significant feature (myr_thb)
+
+linear_model5 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_twd + myr_idr + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_gbp + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
 summary(linear_model5)
 
-# Remove the highest p-value non-significant feature (myr_twd)
+# Remove the highest p-value non-significant feature (myr_gbp)
 
-linear_model6 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_idr+myr_hkd+myr_krw+
-                      myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_try+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
+linear_model6 <- lm(formula = myr_usd ~ myr_rmb + myr_sgd + myr_eur + myr_twd + myr_idr + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
 summary(linear_model6)
+
+# Remove the highest p-value non-significant feature (myr_rmb)
+
+linear_model7 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_twd + myr_idr + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model7)
+
+# Remove the highest p-value non-significant feature (myr_idr)
+
+linear_model8 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_twd + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_try + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model8)
+
+# Remove the highest p-value non-significant feature (myr_try)
+
+linear_model9 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_twd + myr_hkd + myr_krw +
+                      myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_brl + myr_mxn +
+                      myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model9)
 
 # linear_model6 already contains all significant features with threshold of 0.05
 # Continue to remove features until 0.001 is achieved
 
-# Remove the highest p-value non-significant feature (myr_try)
+# Remove the highest p-value non-significant feature (myr_twd)
 
-linear_model7 <- lm(formula=myr_usd~myr_rmb+myr_sgd+myr_eur+myr_idr+myr_hkd+myr_krw+
-                      myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
-summary(linear_model7)
-
-# Remove the highest p-value non-significant feature (myr_rmb)
-
-linear_model8 <- lm(formula=myr_usd~myr_sgd+myr_eur+myr_idr+myr_hkd+myr_krw+
-                      myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
-summary(linear_model8)
-
-# Remove the highest p-value non-significant feature (myr_idr)
-
-linear_model9 <- lm(formula=myr_usd~myr_sgd+myr_eur+myr_hkd+myr_krw+
-                      myr_inr+myr_aud+myr_php+myr_aed+myr_sar+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
-summary(linear_model9)
+linear_model10 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_hkd + myr_krw +
+                       myr_inr + myr_aud + myr_php + myr_aed + myr_sar + myr_brl + myr_mxn +
+                       myr_bdt + myr_chf + myr_cad, data = train)
+summary(linear_model10)
 
 # Remove the highest p-value non-significant feature (myr_aud)
 
-linear_model10 <- lm(formula=myr_usd~myr_sgd+myr_eur+myr_hkd+myr_krw+
-                      myr_inr+myr_php+myr_aed+myr_sar+myr_gbp+myr_brl+myr_mxn+
-                      myr_bdt+myr_chf+myr_cad, data = train_exchange)
-summary(linear_model10)
-
-# Remove the highest p-value non-significant feature (myr_gbp)
-
-linear_model11 <- lm(formula=myr_usd~myr_sgd+myr_eur+myr_hkd+myr_krw+
-                       myr_inr+myr_php+myr_aed+myr_sar+myr_brl+myr_mxn+
-                       myr_bdt+myr_chf+myr_cad, data = train_exchange)
+linear_model11 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_hkd + myr_krw +
+                       myr_inr + myr_php + myr_aed + myr_sar + myr_brl + myr_mxn +
+                       myr_bdt + myr_chf + myr_cad, data = train)
 summary(linear_model11)
 
 # Remove the highest p-value non-significant feature (myr_krw)
 
-linear_model12 <- lm(formula=myr_usd~myr_sgd+myr_eur+myr_hkd+
-                       myr_inr+myr_php+myr_aed+myr_sar+myr_brl+myr_mxn+
-                       myr_bdt+myr_chf+myr_cad, data = train_exchange)
+linear_model12 <- lm(formula = myr_usd ~ myr_sgd + myr_eur + myr_hkd +
+                       myr_inr + myr_php + myr_aed + myr_sar + myr_brl + myr_mxn +
+                       myr_bdt + myr_chf + myr_cad, data = train)
 summary(linear_model12)
 
 # Section IV: Model Evaluation ====
-
 #define list of models
 models <- list(linear_model1, linear_model2, linear_model3, linear_model4, linear_model5, linear_model6,
                linear_model7, linear_model8, linear_model9, linear_model10, linear_model11, linear_model12)
@@ -199,12 +191,13 @@ mod.names <- c('Model 1', 'Model 2', 'Model 3', 'Model 4', 'Model 5', 'Model 6',
 #calculate AIC of each model
 aictab(cand.set = models, modnames = mod.names)
 
-# The best model based on AIC is: Model 5 with 21 features
+# The best model based on AIC is: Model 6
 
 # Evaluate the selected linear model on the test set
-test_predictions <- predict(linear_model5, newdata = test_exchange)
-mse <- mean((test_exchange$myr_usd - test_predictions)^2)
-r_squared <- 1 - (mse / var(test_exchange$myr_usd))
+test_predictions <- predict(linear_model6, newdata = test)
+mse <- mean((test$myr_usd - test_predictions)^2)
+r_squared <- 1 - (mse / var(test$myr_usd))
 
 # Display evaluation metrics
 cat("Model MSE:", mse, "\n")
+cat("Model R Squared:", r_squared, "\n")
